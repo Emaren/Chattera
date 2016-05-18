0d6cbaf105459a11a9a4032f4476d750b2e8e48a
Tony <tonyblum@me.com>
2016-05-17 19:44:08 -0700

    configure chat channel, create _message
    	modified:   app/assets/javascripts/channels/chat.coffee
    	modified:   app/channels/chat_channel.rb
    	new file:   app/views/messages/_message.html.erb

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,13 +1,19 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "messages"
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  def speak(data)
+    ActionCable.server.broadcast('messages',
+    message: render_message(data['message']))
   end
 
-  def speak
+  private
+
+  def render_message(message)
+    ApplicationController.render(partial: 'messages/message',
+                                 locals: { message: message })
   end
+  
 end
