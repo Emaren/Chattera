3271e3e30515f14b8f7827996077843bac8ebf5a
Tony <tonyblum@me.com>
2016-05-17 19:32:05 -0700

    added sessions & messages controllers with new & index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  
+
+  resources :messages, only: [:index]
+  resources :sessions, only: [:new, :create]
+
+  root 'sessions#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
