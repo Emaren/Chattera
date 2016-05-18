357bbe18d66e1ee25ec27f08283604035ee6b69d
Tony <tonyblum@me.com>
2016-05-17 19:37:15 -0700

    added channel, turned on cable connection, enable ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   root 'sessions#new'
 
+  mount ActionCable.server => '/cable'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
